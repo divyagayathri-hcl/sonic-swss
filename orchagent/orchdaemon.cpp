a1cc00a1cafdb9c555ddeccda590562d6f6f33af
mint570 <runmingwu@google.com>
2026-02-20 19:46:58 +0000

    [P4Orch] Enable zmq in p4orch. Signed-off-by: mint570 <runmingwu@google.com> Signed-off-by: divyagayathri-hcl <divyagayathri.s@hcl.com>

--- orchagent/orchdaemon.cpp
+++ orchagent/orchdaemon.cpp
@@ -78,6 +78,9 @@ event_handle_t g_events_handle;
 #define DEFAULT_MAX_BULK_SIZE 1000
 size_t gMaxBulkSize = DEFAULT_MAX_BULK_SIZE;
 
+std::string p4OrchZmqServerEp = "ipc:///zmq_swss/p4orch_zmq_swss_ep";
+swss::ZmqServer* m_p4OrchZmqServer = nullptr;
+
 OrchDaemon::OrchDaemon(DBConnector *applDb, DBConnector *configDb, DBConnector *stateDb, DBConnector *chassisAppDb, ZmqServer *zmqServer) :
         m_applDb(applDb),
         m_configDb(configDb),
@@ -824,8 +827,14 @@ bool OrchDaemon::init()
     m_orchList.push_back(&CounterCheckOrch::getInstance(m_configDb));
 
     vector<string> p4rt_tables = {APP_P4RT_TABLE_NAME};
-    gP4Orch = new P4Orch(m_applDb, p4rt_tables, vrf_orch, gCoppOrch);
+    m_p4OrchZmqServer = new swss::ZmqServer(p4OrchZmqServerEp, /*one_to_one*/true);
+    gP4Orch = new P4Orch(m_applDb, p4rt_tables, m_p4OrchZmqServer, vrf_orch, gCoppOrch);
     m_orchList.push_back(gP4Orch);
+
+    if (m_p4OrchZmqServer)
+    {
+        m_p4OrchZmqServer->bind();
+    }
 
     TableConnector confDbTwampTable(m_configDb, CFG_TWAMP_SESSION_TABLE_NAME);
     TableConnector stateDbTwampTable(m_stateDb, STATE_TWAMP_SESSION_TABLE_NAME);
