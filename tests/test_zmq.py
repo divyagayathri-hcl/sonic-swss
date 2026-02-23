divyagayathri-hcl/sonic-swss

commit 5de59222041bdf29c0be1c9805180333351a9be8

[FC] Set FC delay in command line parameters (#3814)
* [FC] Set FC delay in command line parameters

Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

* static_cast to time_t

Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

* Remove unnecessary whitespace in flexcounterorch.cpp

---------

Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>
Co-authored-by: Sudharsan Dhamal Gopalarathnam <sudharsand@nvidia.com>

orchagent/main.cpp
@@ -103,6 +103,7 @@ void usage()
     cout << "    -v vrf: VRF name (default empty)" << endl;
     cout << "    -I heart_beat_interval: Heart beat interval in millisecond (default 10)" << endl;
     cout << "    -R enable the ring thread feature" << endl;
+    cout << "    -D Delay in seconds before flex counter processing begins after orchagent startup (default 0)" << endl;
 }
 
 void sighup_handler(int signo)
@@ -369,7 +370,7 @@ int main(int argc, char **argv)
     int record_type = 3; // Only swss and sairedis recordings enabled by default.
     long heartBeatInterval = HEART_BEAT_INTERVAL_MSECS_DEFAULT;
 
-    while ((opt = getopt(argc, argv, "b:m:r:f:j:d:i:hsz:k:q:c:t:v:I:R")) != -1)
+    while ((opt = getopt(argc, argv, "b:m:r:f:j:d:i:hsz:k:q:c:t:v:I:R:D:")) != -1)
     {
         switch (opt)
         {
@@ -487,6 +488,7 @@ int main(int argc, char **argv)
         case 'R':
             gRingMode = true;
             break;
+        case 'D': { gFlexCounterDelaySec = swss::to_int<int>(optarg); } break;
         default: /* '?' */
             exit(EXIT_FAILURE);
         }

orchagent/flexcounterorch.cpp
@@ -39,7 +39,7 @@ extern Srv6Orch *gSrv6Orch;
 extern SwitchOrch *gSwitchOrch;
 extern sai_object_id_t gSwitchId;
 
-#define FLEX_COUNTER_DELAY_SEC 60
+int gFlexCounterDelaySec;
 
 #define BUFFER_POOL_WATERMARK_KEY   "BUFFER_POOL_WATERMARK"
 #define PORT_KEY                    "PORT"
@@ -116,11 +116,12 @@ FlexCounterOrch::FlexCounterOrch(DBConnector *db, vector<string> &tableNames):
         SWSS_LOG_ERROR("System error reading create_only_config_db_buffers: %s", e.what());
     }
 
-    m_delayTimer = std::make_unique<SelectableTimer>(timespec{.tv_sec = FLEX_COUNTER_DELAY_SEC, .tv_nsec = 0});
-    if (WarmStart::isWarmStart())
+    SWSS_LOG_NOTICE("Counter delay is %d seconds", gFlexCounterDelaySec);
+    if (gFlexCounterDelaySec > 0)
     {
-        m_delayExecutor = std::make_unique<ExecutableTimer>(m_delayTimer.get(), this, "FLEX_COUNTER_DELAY");
-        Orch::addExecutor(m_delayExecutor.get());
+        m_delayTimer = new SelectableTimer(timespec{.tv_sec = static_cast<time_t>(gFlexCounterDelaySec), .tv_nsec = 0});
+        auto delayExecutor = new ExecutableTimer(m_delayTimer, this, "FLEX_COUNTER_DELAY");
+        Orch::addExecutor(delayExecutor);
         m_delayTimer->start();
     }
     else

orchagent/flexcounterorch.h
@@ -11,6 +11,18 @@ extern "C" {
 #include "sai.h"
 }
 
+// Delay in seconds before flex counter processing begins after orchagent startup.
+// 
+// This delay improves boot time by prioritizing data plane configuration over
+// counter initialization. Systems with many ports, priority groups (PGs), and
+// queues require significant time to generate counter maps, which is not
+// immediately necessary during boot.
+// Value of 0 will process flex counters immediately.
+// 
+// Configured via orchagent command line argument: -D <delay_sec>
+// 
+extern int gFlexCounterDelaySec;
+
 const std::string createAllAvailableBuffersStr = "create_all_available_buffers";
 
 class FlexCounterQueueStates
@@ -75,8 +87,7 @@ private:
     Table m_bufferQueueConfigTable;
     Table m_bufferPgConfigTable;
     Table m_deviceMetadataConfigTable;
-    std::unique_ptr<SelectableTimer> m_delayTimer;
-    std::unique_ptr<Executor> m_delayExecutor;
+    SelectableTimer* m_delayTimer;
     std::unordered_set<std::string> m_groupsWithBulkChunkSize;
 
     bool m_createOnlyConfigDbBuffers = false;

tests/mock_tests/flexcounter_ut.cpp
@@ -274,13 +274,7 @@ namespace flexcounter_test
         sai_switch_api = pold_sai_switch_api;
     }
 
-    enum class StartType
-    {
-        Cold,
-        Warm,
-    };
-
-    struct FlexCounterTest : public ::testing::TestWithParam<std::tuple<bool, bool, StartType>>
+    struct FlexCounterTest : public ::testing::TestWithParam<std::tuple<bool, bool, uint32_t>>
     {
         shared_ptr<swss::DBConnector> m_app_db;
         shared_ptr<swss::DBConnector> m_config_db;
@@ -290,7 +284,6 @@ namespace flexcounter_test
         shared_ptr<swss::DBConnector> m_asic_db;
         shared_ptr<swss::DBConnector> m_flex_counter_db;
         bool create_only_config_db_buffers;
-        StartType m_start_type;
 
         FlexCounterTest()
         {
@@ -317,7 +310,7 @@ namespace flexcounter_test
 
             gTraditionalFlexCounter = get<0>(GetParam());
             create_only_config_db_buffers = get<1>(GetParam());
-            m_start_type = get<2>(GetParam());
+            gFlexCounterDelaySec = get<2>(GetParam());
 
             if (gTraditionalFlexCounter)
             {
@@ -364,17 +357,7 @@ namespace flexcounter_test
                 CFG_FLEX_COUNTER_TABLE_NAME
             };
 
-            if (m_start_type == StartType::Warm)
-            {
-                WarmStart::getInstance().m_enabled = true;
-            }
-
             auto* flexCounterOrch = new FlexCounterOrch(m_config_db.get(), flex_counter_tables);
-
-            if (m_start_type == StartType::Warm)
-            {
-                WarmStart::getInstance().m_enabled = false;
-            }
 
             gDirectory.set(flexCounterOrch);
 
@@ -441,6 +424,9 @@ namespace flexcounter_test
             gDirectory.m_values.clear();
 
             _unhook_sai_switch_api();
+
+            // reset flex counter delay sec
+            gFlexCounterDelaySec = 0;
         }
 
         static void SetUpTestCase()
@@ -641,7 +627,7 @@ namespace flexcounter_test
         flexCounterOrch->addExistingData(&flexCounterCfg);
         static_cast<Orch *>(flexCounterOrch)->doTask();
 
-        if (m_start_type == StartType::Warm)
+        if (gFlexCounterDelaySec > 0)
         {
             // Expire timer
             flexCounterOrch->doTask(*flexCounterOrch->m_delayTimer);
@@ -1015,14 +1001,16 @@ namespace flexcounter_test
         FlexCounterTests,
         FlexCounterTest,
         ::testing::Values(
-            std::make_tuple(false, true, StartType::Cold),
-            std::make_tuple(false, false, StartType::Cold),
-            std::make_tuple(true, true, StartType::Cold),
-            std::make_tuple(true, false, StartType::Cold),
-            std::make_tuple(false, true, StartType::Warm),
-            std::make_tuple(false, false, StartType::Warm),
-            std::make_tuple(true, true, StartType::Warm),
-            std::make_tuple(true, false, StartType::Warm))
+            // traditional_flex_counter, create_only_config_db_buffers, flex_counter_delay_sec
+            std::make_tuple(false, true, 0),
+            std::make_tuple(false, false, 0),
+            std::make_tuple(true, true, 0),
+            std::make_tuple(true, false, 0),
+            std::make_tuple(false, true, 120),
+            std::make_tuple(false, false, 120),
+            std::make_tuple(true, true, 120),
+            std::make_tuple(true, false, 120)
+        )
     );
 
     using namespace mock_orch_test;

commit ebf8c73fe36e2b0b8f5c6b63a3af59b6063377f0

[orchagent] Fix getopt string for -R option (ring_thread_enabled regression) (#4207)
What I did
Fixed a typo in the getopt option string in orchagent/main.cpp where the -R option was incorrectly defined as requiring a mandatory argument (R:) instead of being a simple flag (R).

The fix changes line 403 from:

while ((opt = getopt(argc, argv, "b:m:r:f:j:d:i:hsz:k:q:c:t:v:I:R:D:M")) != -1)
to:

while ((opt = getopt(argc, argv, "b:m:r:f:j:d:i:hsz:k:q:c:t:v:I:RD:M")) != -1)
Why I did it

When ring_thread_enabled is set to true in DEVICE_METADATA, orchagent fails to start with the error:
/usr/bin/orchagent: option requires an argument -- 'R'

This bug was introduced in PR #3814 (specifically, commit 5de5922) when the -D option was added.

The getopt string was changed from I:R to I:R:D: instead of the correct I:RD:.

This accidentally added a colon after R, making it require a mandatory argument.

orchagent/main.cpp
@@ -400,7 +400,7 @@ int main(int argc, char **argv)
     // Disable SAI MACSec POST by default. Use option -M to enable it.
     bool macsec_post_enabled = false;
 
-    while ((opt = getopt(argc, argv, "b:m:r:f:j:d:i:hsz:k:q:c:t:v:I:R:D:M")) != -1)
+    while ((opt = getopt(argc, argv, "b:m:r:f:j:d:i:hsz:k:q:c:t:v:I:RD:M")) != -1)
     {
         switch (opt)
         {

tests/test_zmq.py
@@ -154,3 +154,20 @@ class TestZmqDash(object):
         dvs.runcmd("cp /usr/bin/orchagent.sh_usage_ut_backup /usr/bin/orchagent.sh")
         dvs.stop_swss()
         dvs.start_swss()
+
+    def test_ring_thread(self, dvs):
+        # Improve test code coverage, change orchagent to enable ring thread
+        dvs.runcmd("cp /usr/bin/orchagent.sh /usr/bin/orchagent.sh_ring_ut_backup")
+        dvs.runcmd("sed -i.bak 's/\/usr\/bin\/orchagent /\/usr\/bin\/orchagent -R /g' /usr/bin/orchagent.sh")
+        dvs.stop_swss()
+        dvs.start_swss()
+
+        # wait orchagent start
+        time.sleep(3)
+        process_statue = dvs.runcmd("ps -ef")
+        zmq_logger.debug("Process status: {}".format(process_statue))
+
+        # revert change
+        dvs.runcmd("cp /usr/bin/orchagent.sh_ring_ut_backup /usr/bin/orchagent.sh")
+        dvs.stop_swss()
+        dvs.start_swss()